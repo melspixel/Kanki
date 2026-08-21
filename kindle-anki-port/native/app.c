e5fc6dd255ad490d6596de1a1050a917dc42a905
melspixel <melquiades13141@gmail.com>
2026-08-21 23:38:15 +0800

    source: materialize native host translation unit

---
+++ kindle-anki-port/native/app.c
@@ -0,0 +1,12 @@
+/*
+ * Kindle native host translation unit.
+ *
+ * The implementation is split into ordinary textual include fragments only
+ * to keep the GitHub source materialization transaction manageable.  The
+ * fragments are compiled in order and together are byte-for-byte equivalent
+ * to the verified VM checkpoint implementation after preprocessing.
+ */
+#include "app_part1.inc"
+#include "app_part2.inc"
+#include "app_part3.inc"
+#include "app_part4.inc"
