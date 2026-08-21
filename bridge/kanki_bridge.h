b5c587969f90d592d187fb8bb7591e5a23a456d3
melspixel <melquiades13141@gmail.com>
2026-08-21 13:28:18 +0800

    Expose typed answer preparation in semantic ABI

--- bridge/kanki_bridge.h
+++ bridge/kanki_bridge.h
@@ -25,6 +25,7 @@ char *kanki_deck_tree_json(KankiCore *core);
 char *kanki_set_current_deck_json(KankiCore *core, int64_t deck_id);
 char *kanki_set_deck_collapsed_json(KankiCore *core, int64_t deck_id, uint8_t collapsed);
 char *kanki_next_card_json(KankiCore *core);
+char *kanki_prepare_answer_json(KankiCore *core, const char *typed_answer);
 char *kanki_answer_json(KankiCore *core, uint32_t rating, uint32_t milliseconds_taken);
 char *kanki_bury_current_json(KankiCore *core);
 char *kanki_health_json(KankiCore *core);
