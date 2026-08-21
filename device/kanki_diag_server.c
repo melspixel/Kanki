07052a159ac158a21688749ff01c49773819e465
melspixel <melquiades13141@gmail.com>
2026-08-21 19:57:36 +0800

    fix: bind diagnostics portably to loopback

--- device/kanki_diag_server.c
+++ device/kanki_diag_server.c
@@ -320,7 +320,10 @@ int main(void) {
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_port = htons(PORT);
-    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (inet_pton(AF_INET, "127.0.0.1", &address.sin_addr) != 1) {
+        close(server);
+        return 75;
+    }
     if (bind(server, (struct sockaddr *)&address, sizeof(address)) != 0) {
         fprintf(stderr, "kanki-diag: bind failed: %s\n", strerror(errno));
         close(server);
